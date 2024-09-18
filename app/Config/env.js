02bbb0e72ddc6bc665487f9fb750c8e1769eb019
Joycee Aleman <alemanjoycee@gmail.com>
2024-09-18 10:36:03 -0600

    Joycee Aleman: Inicio de proyecto

---
+++ app/Config/env.js
@@ -0,0 +1,16 @@
+const env = {
+  database: 'umg_antigua_7290_05',
+  username: 'umg_antigua_7290_05_user',
+  password: 'V9w968aCQaZ27Ovya1jisS2AwcNQcOgv',
+  host: 'dpg-crkejpggph6c73c8v8l0-a.oregon-postgres.render.com',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
+  
